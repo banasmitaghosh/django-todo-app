169c86bc6b165515c17daebbaab75329a5e646c7
Banasmita Ghosh <109519831+banasmitaghosh@users.noreply.github.com>
2023-09-04 00:10:43 +0530

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/', 'http://localhost:8000/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
