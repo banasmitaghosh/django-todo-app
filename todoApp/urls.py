44165f4cdfcdfa4aa6d667c2788490160b8b7f40
Banasmita Ghosh <109519831+banasmitaghosh@users.noreply.github.com>
2023-09-04 00:01:07 +0530

    Update urls.py

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -20,7 +20,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import settings, views
 
 urlpatterns = [
-    path('todos/', include('todos.urls')),
+    path('', include('todos.urls')),
     path('admin/', admin.site.urls),
     path('', views.index)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
